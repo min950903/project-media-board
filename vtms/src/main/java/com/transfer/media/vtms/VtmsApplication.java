e4cc6def719d7554ae8aa43466aedd2736541555
min950903 <min950903@gmail.com>
2020-03-16 09:46:59 +0900

    springboot init

---
+++ vtms/src/main/java/com/transfer/media/vtms/VtmsApplication.java
@@ -0,0 +1,13 @@
+package com.transfer.media.vtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtmsApplication.class, args);
+	}
+
+}
---
+++ vtms/src/test/java/com/transfer/media/vtms/VtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfer.media.vtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
